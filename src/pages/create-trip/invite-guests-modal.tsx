e83b03279982a64776d48252f23a3f6f9665fb57
rafamattia2 <rafaeldemattia.rm@gmail.com>
2024-07-11 16:16:08 -0300

    ✨ feature: steps, confirm and invite modals

--- src/pages/create-trip/invite-guests-modal.tsx
+++ src/pages/create-trip/invite-guests-modal.tsx
@@ -1,5 +1,6 @@
 import { AtSign, Plus, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface InviteGuestsModalProps {
   closeGuestsModal: () => void;
@@ -25,8 +26,7 @@ export function InviteGuestsModal({
             </button>
           </div>
           <p className="text-sm text-zinc-400">
-            Os convidados irão receber e-mails para confirmar a participação na
-            viagem.
+            Os convidados irão receber e-mails para confirmar a participação na viagem.
           </p>
         </div>
 
@@ -38,10 +38,7 @@ export function InviteGuestsModal({
                 className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
               >
                 <span className="text-zinc-300">{email}</span>
-                <button
-                  type="button"
-                  onClick={() => removeEmailFromInvites(email)}
-                >
+                <button type="button" onClick={() => removeEmailFromInvites(email)}>
                   <X className="size-4 text-zinc-400" />
                 </button>
               </div>
@@ -62,13 +59,11 @@ export function InviteGuestsModal({
               className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
             />
           </div>
-          <button
-            type="submit"
-            className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-          >
+
+          <Button variant="primary">
             Convidar
             <Plus className="text-lime-950 size-5" />
-          </button>
+          </Button>
         </form>
       </div>
     </div>
--- src/pages/create-trip/steps/invite-guests-step.tsx
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, UserRoundPlus } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface InviteGuestsStepProps {
   openGuestsModal: () => void;
@@ -23,19 +24,16 @@ export function InviteGuestsStep({
             {emailsToInvite.length} pessoa(s) convidada(s)
           </span>
         ) : (
-          <span className="text-zinc-400 text-lg flex-1 text-left">
-            Quem estará na viagem?
-          </span>
+          <span className="text-zinc-400 text-lg flex-1 text-left">Quem estará na viagem?</span>
         )}
       </button>
+
       <div className="w-px h-6 bg-zinc-800"></div>
-      <button
-        onClick={openConfirmTripModal}
-        className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-      >
+
+      <Button onClick={openConfirmTripModal} variant="primary">
         Confirmar viagem
         <ArrowRight className="text-lime-950 size-5" />
-      </button>
+      </Button>
     </div>
   );
 }
